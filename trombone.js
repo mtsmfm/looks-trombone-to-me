6a083734e44edb60155cde6b7ba6dd452d42f01d
Seiei Higa <hanachin@gmail.com>
2014-04-22 01:35:32 +0900

    more private

--- trombone.js
+++ trombone.js
@@ -1,8 +1,8 @@
 window.trombonize = function(parentSelector, bodySelector) {
-  var trombone = '<img src="https://camo.githubusercontent.com/24704947d08221825eb967a0009f5dc361a4adb6/687474703a2f2f7777772e6c67746d2e696e2f702f50527732" width=200/>';
   $('html').livequery(
     parentSelector,
     function(elem) {
+      var trombone = '<img src="https://camo.githubusercontent.com/24704947d08221825eb967a0009f5dc361a4adb6/687474703a2f2f7777772e6c67746d2e696e2f702f50527732" width=200/>';
       $(elem).find(bodySelector).each(function(){
         $(this).html($(this).html().replace(/(:trombone:)(?=[^>]*(?:<|$))/, trombone));
       })
