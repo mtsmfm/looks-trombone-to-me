db3d609ce96b050e49a819f63cf9674c86086acf
Seiei Higa <hanachin@gmail.com>
2014-04-22 02:11:45 +0900

    CleanCode

--- jquery.trombonize.js
+++ jquery.trombonize.js
@@ -1,7 +1,7 @@
 (function ($) {
-  var trombone_url = 'https://camo.githubusercontent.com/24704947d08221825eb967a0009f5dc361a4adb6/687474703a2f2f7777772e6c67746d2e696e2f702f50527732';
-  var trombone_img_tag = '<img src="' + trombone_url + '" width=200/>';
   $.fn.trombonize = function () {
+    var trombone_url = 'https://camo.githubusercontent.com/24704947d08221825eb967a0009f5dc361a4adb6/687474703a2f2f7777772e6c67746d2e696e2f702f50527732';
+    var trombone_img_tag = '<img src="' + trombone_url + '" width=200/>';
     $(this).html($(this).html().replace(/(:trombone:)(?=[^>]*(?:<|$))/, trombone_img_tag));
     return this;
   };
