dc74f02022c136e23a0565faebb953a2b5fa2909
Seiei Higa <hanachin@gmail.com>
2014-04-22 09:15:47 +0900

    CleanCode

--- github/trombone.js
+++ github/trombone.js
@@ -2,9 +2,7 @@ $(function () {
   $('body').livequery(
     'div',
     function(elem) {
-      $(elem).find('.comment-body:contains(":trombone:")').each(function () {
-        $(this).trombonize();
-      });
+      $(elem).find('.comment-body:contains(":trombone:")').trombonize();
     }
   );
 });
