1eecd59b1ac20a14c175fd0c3c326373e28159f8
Seiei Higa <hanachin@gmail.com>
2014-04-22 10:19:42 +0900

    Extract livetrombone

--- jquery.trombonize.js
+++ jquery.trombonize.js
@@ -7,18 +7,4 @@
     });
     return this;
   };
-
-  $.fn.livetrombone = function (options) {
-    var settings = $.extend({
-      parentSelector: 'div',
-      bodySelector: '*'
-    }, options);
-
-    $(this).livequery(
-      settings.parentSelector,
-      function(elem) {
-        $(elem).find(settings.bodySelector).filter(':contains(":trombone:")').trombonize();
-      }
-    );
-  };
 })(jQuery);
